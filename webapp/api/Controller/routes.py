f8edd8ae5a1269b6ea17b97f1aa3b72bbd54c3cf
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2020-05-14 22:35:11 +0200

    adding project

---
+++ webapp/api/Controller/routes.py
@@ -0,0 +1,13 @@
+from .routesfunc import *
+
+def setuproute(app, call):
+    @app.route('/test/',                ['OPTIONS', 'POST', 'GET'], lambda x = None: call([])                                        )
+    @app.route('/login/',    	        ['OPTIONS', 'POST'],        lambda x = None: call([getauth])                                 )
+    @app.route('/user/signup/',    	    ['OPTIONS', 'POST'],        lambda x = None: call([myauth, signup, signin, gettoken])        )
+    @app.route('/user/signin/',    	    ['OPTIONS', 'POST'],        lambda x = None: call([myauth, signin, gettoken])                )
+    @app.route('/user/renew/',    	    ['OPTIONS', 'GET'],         lambda x = None: call([myauth, authuser, gettoken])              )
+    @app.route('/token/send/',    	    ['OPTIONS', 'POST'],        lambda x = None: call([myauth, authuser, sendtok])               )
+    @app.route('/wallet/',    	        ['OPTIONS', 'GET'],         lambda x = None: call([myauth, authuser, wallet])                )
+    @app.route('/wallet/cred/',    	    ['OPTIONS', 'GET'],         lambda x = None: call([myauth, authuser, walletcred])            )
+    def base():
+        return
---
+++ webapp/api/Controller/routesfunc.py
@@ -0,0 +1,84 @@
+from Model.basic import check, auth
+from Object.users import user
+import json
+
+def getauth(cn, nextc):
+    err = check.contain(cn.pr, ["pass"])
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
+    cn.pr = err[1]
+    err = auth.gettoken(cn.pr["pass"])
+    return cn.call_next(nextc, err)
+
+def myauth(cn, nextc):
+    err = check.contain(cn.hd, ["token"], "HEAD")
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
+    cn.hd = err[1]
+    err = auth.verify(cn.hd["token"])
+    return cn.call_next(nextc, err)
+
+def signup(cn, nextc):
+    err = check.contain(cn.pr, ["email", "password1", "password2"])
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
+    cn.pr = err[1]
+
+    use = user()
+    err = use.register(cn.pr["email"], cn.pr["password1"], cn.pr["password2"])
+    cn.private["user"] = use
+
+    return cn.call_next(nextc, err)
+
+def signin(cn, nextc):
+    err = check.contain(cn.pr, ["email", "password1"])
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
+    cn.pr = err[1]
+
+    use = user()
+    err = use.login(cn.pr["email"], cn.pr["password1"])
+    cn.private["user"] = use
+
+    return cn.call_next(nextc, err)
+
+def authuser(cn, nextc):
+    err = check.contain(cn.hd, ["usrtoken"], "HEAD")
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
+    cn.hd = err[1]
+
+    use = user()
+    err = use.verify(cn.hd["usrtoken"])
+    cn.private["user"] = use
+
+    return cn.call_next(nextc, err)
+
+def gettoken(cn, nextc):
+    err = check.contain(cn.pr, [])
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
+    cn.pr = err[1]
+
+    use = cn.private["user"]
+    err = use.gettoken()
+    return cn.call_next(nextc, err)
+
+
+def sendtok(cn, nextc):
+    err = check.contain(cn.pr, ["to", "curr", "date", "amount"])
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
+    cn.pr = err[1]
+
+    err = [True, {"transaction" : {"type": "send", "amount": cn.pr["to"], "curr": cn.pr["curr"], "to": cn.pr["to"], "date": int(cn.pr["date"])}}, None]
+    return cn.call_next(nextc, err)
+
+def wallet(cn, nextc):
+    err = [True, {"tokens": {"eth": 1.00000214, "ex2": 23.0000002}, "transactions": [{"type": "send", "amount": 2.23, "curr": "eth", "to": "eliot.courtel@wanadoo.fr", "date": 1589487889}, {"type": "send", "amount": 0.523, "curr": "eth", "to": "jhlauret@gmail.com", "date": 1589487889 }]}, None]
+    return cn.call_next(nextc, err)
+
+def walletcred(cn, nextc):
+    err = [True, {"public": "uijbszdkjbsdojbsd", "private": "XXXXXXxxx"}, None]
+
+    return cn.call_next(nextc, err)
